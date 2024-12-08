292fff4115e26bce87f62e0dc07ed5cb88d3e79c
ZeroX-DG <viethungax@gmail.com>
2024-12-08 20:24:48 +1300

    added day7 part 2

--- day7/day7.exs
+++ day7/day7.exs
@@ -14,11 +14,18 @@ defmodule Day7 do
     case operator do
       :+ -> eval_equation([a + b | rest])
       :* -> eval_equation([a * b | rest])
+      :|| -> eval_equation([String.to_integer("#{a}#{b}") | rest])
     end
   end
 
-  def verify_equation(test_value, equation) do
+  def verify_equation_part1(test_value, equation) do
     build_equations(equation, [:+, :*], [])
+    |> Enum.map(&eval_equation/1)
+    |> Enum.any?(fn result -> result == test_value end)
+  end
+
+  def verify_equation_part2(test_value, equation) do
+    build_equations(equation, [:+, :*, :||], [])
     |> Enum.map(&eval_equation/1)
     |> Enum.any?(fn result -> result == test_value end)
   end
@@ -38,10 +45,31 @@ defmodule Day7 do
       end)
 
     equations
-      |> Enum.filter(fn {test_value, equation} -> verify_equation(test_value, equation) end)
+      |> Enum.filter(fn {test_value, equation} -> verify_equation_part1(test_value, equation) end)
+      |> Enum.reduce(0, fn {test_value, _}, acc -> acc + test_value end)
+      |> IO.inspect 
+  end
+
+  def part2 do
+    file_path = "input.txt"
+
+    lines =
+      file_path
+      |> File.read!()
+      |> String.split("\n", trim: true)
+
+    equations = lines
+      |> Enum.map(fn line ->
+        [test_value, equation] = String.split(line, ":")
+        { String.to_integer(test_value), String.split(equation, " ", trim: true) |> Enum.map(&String.to_integer/1) }
+      end)
+
+    equations
+      |> Enum.filter(fn {test_value, equation} -> verify_equation_part2(test_value, equation) end)
       |> Enum.reduce(0, fn {test_value, _}, acc -> acc + test_value end)
       |> IO.inspect 
   end
 end
 
 Day7.part1
+Day7.part2
